c2c4c993d0f295c0ee9f378bbbdb7f77bcad8413
Soviet Cat <klumzydukgamez@outlook.com>
2026-03-23 09:03:55 +1000

    Reset Before API Restructure 23/03/2026
    Removed Include/Klumzy.h
    Removed Include/Klumzy_Common.h
    Removed Include/Klumzy_Display.h
    Removed Include/Klumzy_Math.h
    Removed Include/Klumzy_Result.h
    Removed Inclue/Klumzy_Math.h
    Removed Include/Klumzy_Result.h
    Changed Source/Editor/Editor_main.c
    Removed Source/Klumzy_Display.c
    Removed Source/main.c

--- Include/Klumzy.h
+++
@@ -1,8 +0,0 @@
-/* This file is apart of KlumzyEngine and is property of Klumzy Duk Gamez
- * Copyright (c) 2026 Klumzy Duk Gamez
- * See LICENE.md for license details */
-
-#include "Klumzy_Common.h"
-#include "Klumzy_Result.h"
-#include "Klumzy_Math.h"
-#include "Klumzy_Display.h"
--- Source/Editor/Editor_main.c
+++ Source/Editor/Editor_main.c
@@ -2,35 +2,6 @@
  * Copyright (c) 2026 Klumzy Duk Gamez
  * See LICENSE.md for license details */
 
-#include <Klumzy.h>
-#include <stdio.h>
-
 int main(int argc, char* argv[]) {
-	Klumzy_Display display;
-
-	Klumzy_Result result = Klumzy_StartupDisplay(&display);
-	if (Klumzy_IsResultSuccess(result) == Klumzy_Null) {
-		printf("Failed to startup display: %d\n", result);
-	}
-
-	Klumzy_Window* window;
-	for (Klumzy_UInt32 i = 0; i < 2; i++) {
-		result = Klumzy_OpenWindow(&display, &window);
-		if (Klumzy_IsResultSuccess(result) == Klumzy_Null) {
-			printf("Failed to open window (%d): %d\n", i, result);
-		}
-	}
-
-	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0) != Klumzy_Null) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	result = Klumzy_ShutdownDisplay(&display);
-	if (Klumzy_IsResultSuccess(result) == Klumzy_Null) {
-		printf("Failed to shutdown display: %lu\n", GetLastError());
-	}
-
-	return Klumzy_Result_SUCCESS;
+	return 0;
 }
